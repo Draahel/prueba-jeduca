9fcc7222269b92e01b0bc46c80f4a8692afdbc37
Daniel <merc801566@siigo.com>
2022-10-26 13:22:08 -0500

    change file name

--- src/helpers/createFile.js
+++ src/helpers/createFile.js
@@ -2,8 +2,10 @@
 import { saveAs } from 'file-saver';
 
 export const createFile = (data) => {
+    const filename = `${data.title}-${data.n1}-${data.n2}-${data.n3}-${data.year}-${data.expiration}`;
+    console.log(filename);
     const blob = new Blob([JSON.stringify(data, null, 2)], {
     type: "application/json",
     });
-    saveAs(blob, 'mi-archivo.txt')
-  }
+    saveAs(blob, filename)
+}
